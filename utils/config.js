1364ca97345165ba6b1c6238b977562363fd290a
JUAN ALCANTARA <2481328@gmail.com>
2025-05-15 02:13:10 -0400

    CORS update

--- utils/config.js
+++ utils/config.js
@@ -6,6 +6,7 @@ const corsOptions = {
     "http://localhost:3001",
     "http://localhost:4173",
     "https://whattowear.mine.bz",
+    "https://www.whattowear.mine.bz",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
