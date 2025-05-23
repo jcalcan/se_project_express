04bf3d0c2ac21de70fa5839bf4627c8aca266edf
JUAN ALCANTARA <2481328@gmail.com>
2025-05-22 22:06:10 -0400

    checking for error handling

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -14,7 +14,25 @@ const {
 } = require("../controllers/clothingItems");
 
 router.get("/", getItems);
-router.post("/", auth, validateClothingItem, createItem);
+// router.post("/", auth, validateClothingItem, createItem);
+router.post(
+  "/",
+  (req, res, next) => {
+    console.log("POST /items: hit auth");
+    next();
+  },
+  auth,
+  (req, res, next) => {
+    console.log("POST /items: hit validateClothingItem");
+    next();
+  },
+  validateClothingItem,
+  (req, res, next) => {
+    console.log("POST /items: hit createItem");
+    next();
+  },
+  createItem
+);
 router.delete("/:itemId", auth, validateIds, deleteItem);
 router.put("/:itemId/likes", auth, validateIds, likeItem);
 router.delete("/:itemId/likes", auth, validateIds, unlikeItem);
